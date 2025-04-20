f6456099957cd7e41f6a7e5cdceacced7363ebcb
Berk Polat <berkpolat125@gmail.com>
2025-04-20 12:37:18 +0300

    BS added

---
+++ DS&A/Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_A.Algorithms.Search
+{
+    public class BinarySearch
+    {
+        static int BinarySearchTrueApproach(int[] arr, int start, int end, int key)
+        {
+            while (start <= end) {
+                //int mid = (start + end) / 2; // OVERFLOW!!
+                //int mid = start + ((end - start) / 2); //true approach
+                int mid = start + ((end - start) >> 1); // binary shifting
+
+                if (arr[mid] == key) { return mid; }
+
+                if (arr[mid] > key)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+
+            }
+            return -1;
+
+        }
+    }
+}
