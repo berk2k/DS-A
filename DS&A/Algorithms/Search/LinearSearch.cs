3ee6355c0dd82ae57f977d86692bf6be208f9dfe
Berk Polat <berkpolat125@gmail.com>
2025-04-20 12:28:29 +0300

    linear search added

---
+++ DS&A/Algorithms/Search/LinearSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_A.Algorithms.Search
+{
+    public class LinearSearch
+    {
+        public static int Search<T>(T[] array, T target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(Equals(array[i], target))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    // BEST CASE -> O(1)
+    // WORST CASE -> O(N)
+    // AVG CASE -> O(N/2) -> O(N)
+
+     
+}
