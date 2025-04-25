04ec490fd38f24b8ed1c3b0221e48ca2e82a7a22
Berk Polat <berkpolat125@gmail.com>
2025-04-25 10:07:36 +0300

    selection sort

---
+++ DS&A/Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_A.Algorithms.Sorting
+{
+    public class SelectionSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T> 
+        {
+            for (int i = 0; i < array.Length - 1; i++) {
+                int minIndex = i;
+
+                for (int j = i + 1; j < array.Length; j++) {
+
+                    if (array[j].CompareTo(array[minIndex]) < 0) {
+                        minIndex = j;
+
+                    }
+                
+                }
+                if(minIndex != i)
+                {
+                    T temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+
+                }
+            
+            }
+            
+        
+        }
+
+    }
+}
